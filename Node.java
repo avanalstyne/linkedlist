cf749be3ee989f3dd19abf9e11b3f6d5a592f8fd
avanalstyne <avanalstyne@albany.edu>
2016-10-22 20:34:29 -0400

    comment

--- Node.java
+++ Node.java
@@ -1,3 +1,43 @@
 public class Node{
+
   //properties
-  int
+   private String data;
+   private Node next;
+   private Node prev;
+   private Node current;
+   
+//constructor
+   public Node(String input_data){
+        this.data = input_data;
+        this.prev = null;
+        this.next = null;
+   }
+   
+   //getters
+   public String getData(){
+    return this.data;
+   }
+   //setters
+   public void setData(String input_data){
+    this.data = input_data;
+   }
+   
+   //Node next getter
+   public Node getNext(){
+    return this.next;
+   }
+   //Node next setter
+   public void setNext(Node n){
+    this.next = n;
+   }
+   
+   //Node prev getter
+   public Node getPrev(){
+    return this.prev;
+   }
+   //Node prev setter
+   public void setPrev(Node n){
+    this.prev = n;
+   }
+}
+
